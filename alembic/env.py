98ce5c6f50ef64d20255c415a2efe0b42ecd45a9
MichaelM3 <michael.muniz94@gmail.com>
2023-03-14 13:28:01 -0400

    setup env url for alembic

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -5,9 +6,17 @@ from sqlalchemy import pool
 
 from alembic import context
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+# pass environment variables
+if not DATABASE_URL:
+    raise ValueError("ENVIRONMENT VARIABLE NOT SET")
+section = config.config_ini_section
+config.set_section_option(section, "DATABASE_URL", DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
