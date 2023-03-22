f4bb702f0a830ba6be9397bb704ee60edbcba3d7
MichaelM3 <michael.muniz94@gmail.com>
2023-03-22 15:48:54 -0400

    added get_channel function to crud

--- app/crud/crud_channel.py
+++ app/crud/crud_channel.py
@@ -4,5 +4,11 @@ from ..schemas import channel_schema
 from ..db.models import channel_model
 
 def get_channels(db: Session):
-   return db.query(channel_model.Channel).all()
+    return db.query(channel_model.Channel).all()
     
+def get_channel(id: int, db: Session):
+    channel = db.query(channel_model.Channel).filter(channel_model.Channel.id == id).first()
+    if not channel:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No channel found with id of {id}")
+    return channel
+
